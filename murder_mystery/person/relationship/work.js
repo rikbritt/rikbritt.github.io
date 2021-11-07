175c59de0749f32359c4cd6e37d8109265d7a3ba
rikbritt <richard.britt@live.co.uk>
2021-11-07 09:33:03 +0000

    More missing files in mm relationship

---
+++ murder_mystery/person/relationship/work.js
@@ -0,0 +1,25 @@
+var mm_workRelationshipGenerator = {
+	version:1,
+	name:"Work",
+	category:"Murder Mystery / Person / Relationship",
+	inputs:{
+		person1:{				type:"data",		dataType:mm_personDataDef },
+		person2:{				type:"data",		dataType:mm_personDataDef },
+		relationship1To2:{		type:"data",		dataType:mm_relationshipDataDef	},
+		relationship2To1:{		type:"data",		dataType:mm_relationshipDataDef	}
+	},
+	outputs:{
+		relationship1To2:{		type:"data",		dataType:mm_relationshipDataDef	},
+		relationship2To1:{		type:"data",		dataType:mm_relationshipDataDef	}
+	},
+	script:function(inputs, outputs){
+		outputs.relationship1To2 = inputs.relationship1To2;
+		outputs.relationship2To1 = inputs.relationship2To1;
+		
+		outputs.relationship1To2.type = "Work";
+		outputs.relationship2To1.type = "Work";
+		
+		
+	}
+}
+bg.RegisterGenerator(mm_workRelationshipGenerator);
