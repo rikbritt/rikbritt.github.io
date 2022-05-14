6acf5e231680e1ed83de1c046e8b92a830a47868
rikbritt <richard.britt@live.co.uk>
2022-05-14 12:36:25 +0100

    Add mm scenario 2

--- murder_mystery/scenario.js
+++ murder_mystery/scenario.js
@@ -1,3 +1,56 @@
+var mm_scenarioGenerator2 = {
+	version:1,
+	name:"Scenario V2",
+	description:"Relationships First, Then People",
+	category:["Murder Mystery"],
+	inputs:{
+		numPeople:{		type:"int", min:3, max:20	}
+	},
+	outputs:{
+		data:{		type:"data",		dataType:mm_personDataDef	} /*fix type*/
+	},
+	script:function(inputs, outputs)
+	{
+		outputs.data = inputs;
+		outputs.data.people = [];
+		for(var i=0; i<inputs.numPeople; ++i)
+		{
+			var person = bg.RunGenerator(mm_personGenerator, inputs.seed+i, {}).outputs.data;
+			outputs.data.people.push(person);
+		}
+		outputs.data.victim = outputs.data.people[0];
+		outputs.data.victimCauseOfDeath = bg.RunGenerator(mm_causeOfDeathGenerator, inputs.seed, {}).outputs.data;
+		outputs.data.suspects = [];
+		outputs.data.murderer = 1;//outputs.data.people[1];
+		
+		//Relationships
+		outputs.data.relationships = [];
+		for(var i=0; i<inputs.numPeople; ++i)
+		{
+			for(var j=0; j<inputs.numPeople; ++j)
+			{
+				if(i==j)
+				{
+					continue;
+				}
+				
+				var relationshipInputs = {
+					person1:outputs.data.people[i],
+					person2:outputs.data.people[j],
+				};
+				
+				var relationships = bg.RunGenerator(mm_relationshipGenerator, inputs.seed+i, relationshipInputs).outputs;
+				
+				if(j==0)
+				{
+					outputs.data.relationships.push(relationships.relationship1To2);
+				}
+			}
+		}
+	}
+}
+bg.RegisterGenerator(mm_scenarioGenerator2);
+
 var mm_scenarioGenerator = {
 	version:1,
 	name:"Scenario V1",
