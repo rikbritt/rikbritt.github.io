18412b5349170760d652acaeedde6bec2c64d988
rikbritt <richard.britt@live.co.uk>
2022-05-15 20:45:45 +0100

    mm_relationshipsGenerator

--- murder_mystery/person/relationship/relationship.js
+++ murder_mystery/person/relationship/relationship.js
@@ -13,8 +13,8 @@ var mm_relationshipsGenerator = {
 	},
 	script:function(inputs, outputs){
 		
-		outputs.relationship1To2 = bg.BuildDataFields(mm_relationshipDataDef, 0, null);
-		outputs.relationship2To1 = bg.BuildDataFields(mm_relationshipDataDef, 0, null);
+		outputs.relationship1To2 = bg.BuildDataFields(mm_relationshipDataDef.fields, inputs.seed + 1, null);
+		outputs.relationship2To1 = bg.BuildDataFields(mm_relationshipDataDef.fields, inputs.seed + 1, null);
 	}
 }
 bg.RegisterGenerator(mm_relationshipsGenerator);
