49e8a98d94a878cb28a4e28d0e1da7c045fd6916
rikbritt <richard.britt@live.co.uk>
2022-05-16 19:45:44 +0100

    relationship tags

--- murder_mystery/person/relationship/relationship_datadefs.js
+++ murder_mystery/person/relationship/relationship_datadefs.js
@@ -2,7 +2,7 @@ var mm_relationshipDataDef = {
 	version:1,
 	name:"Relationship",
 	fields:{
-		type:{			type:"text" },
+		tags:{			type:"list", 	elementType:{ type:"text" } },
 		friendship:{	type:"norm",	min:0,	max:1 },
 		respect:{		type:"norm",	min:0,	max:1 },
 		attraction:{	type:"norm",	min:0,	max:1 }
