bf1a6133ea3354552bb1c41faac0ed6417834db9
rikbritt <richard.britt@live.co.uk>
2022-07-19 18:51:16 +0100

    working om timeline imgui

--- webui/imgui_timeline.js
+++ webui/imgui_timeline.js
@@ -6,7 +6,10 @@ function CreateTimelineContext()
         y:ImGui.GetCursorScreenPos().y,
         GetTimeX:function(t) {
             return this.x + (t * 4);
-        }
+        },
+        stream_height:20,
+        stream_y:0,
+        range_col:0xFFA49780
     };
     
     return ctx;
@@ -14,21 +17,48 @@ function CreateTimelineContext()
 
 function DrawTimelineRange(ctx, name, start, end)
 {
-    ctx.draw.AddRectFilled({x:ctx.GetTimeX(start),y:ctx.y},{x:ctx.GetTimeX(end),y:ctx.y+20},0xFFFFFFFF );
-    ctx.draw.AddText({x:ctx.GetTimeX(start), y:ctx.y+5}, 0xFFFFFF00, name);
+    var ew = ctx.stream_height / 4; //end width
+    var hh = ctx.stream_height / 2; //half height
+    var ry = ctx.stream_y + ctx.y; //range y
+    var rx = ctx.GetTimeX(start); //range x
+    var bx = rx + ew; //body start x
+    var bex = ctx.GetTimeX(end) - ew; //body end x
+    ctx.draw.AddConvexPolyFilled(
+        [
+            {x:bx, y:ry + ctx.stream_height},
+            {x:rx, y:ry + hh},
+            {x:bx, y:ry},
+            {x:bex,y:ry},
+            {x:ctx.GetTimeX(end), y:ry + hh},
+            {x:bex, y:ry + ctx.stream_height}
+        ], 
+        6, 
+        ctx.range_col
+    );
+    
+    ctx.draw.PushClipRect({x:bx,y:ctx.y}, {x:bex, y:ry + ctx.stream_height});
+    //ctx.draw.AddRectFilled({x:ctx.GetTimeX(start),y:ctx.y},{x:ctx.GetTimeX(end),y:ctx.y+20},0xFFFFFFFF );
+    ctx.draw.AddText({x:bx, y:ry+2}, 0xFF0000FF, name);
+    ctx.draw.PopClipRect();
 }
 
 function AddTimeline(id, timeline)
 {
     ImGui.BeginChild(id, new ImGui.Vec2(-1,-1), true, ImGui.ImGuiWindowFlags.HorizontalScrollbar)
-    ImGui.BeginChild("TimelineScrollArea", new ImGui.Vec2(500,60));
+    ImGui.BeginChild("TimelineScrollArea", new ImGui.Vec2(500,100));
     var draw_list = ImGui.GetWindowDrawList();
     var p = ImGui.GetCursorScreenPos();
 
     var ctx = CreateTimelineContext();
-    DrawTimelineRange(ctx, "Test Range", 5, 20);
+    DrawTimelineRange(ctx, "Test Range", 5, 25);
+    
+    ctx.stream_y = 30;
+    DrawTimelineRange(ctx, "Test Range 2", 15, 50);
+    
+    ctx.stream_y = 60;
+    DrawTimelineRange(ctx, "Test Range 3", 15, 50);
 
-    draw_list.AddLine({x:p.x+5,y:p.y+5}, {x:p.x+210,y:p.y+220},0xFFFFFFFF);
+    //draw_list.AddLine({x:p.x+5,y:p.y+5}, {x:p.x+210,y:p.y+220},0xFFFFFFFF);
 
     ImGui.EndChild();
     ImGui.EndChild();
