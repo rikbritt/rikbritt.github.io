42030edb8808db0d5e08b6c379e86d842d9ce31e
rikbritt <richard.britt@live.co.uk>
2022-08-19 08:10:19 +0100

    fixed date order

--- tests/timelinetests.js
+++ tests/timelinetests.js
@@ -12,9 +12,9 @@ var testFixedTimelineGenerator = {
     {
         var timeline = bg.TimelineCreate("Test Timeline");
 		var rik_stream = bg.TimelineCreateStream(timeline, "Rik", {age:25});
-        bg.TimelineAddStreamEvent(rik_stream, bg.TimelineDateToTime(1982, 10, 3), "Born", {});
-        bg.TimelineAddStreamEvent(rik_stream, bg.TimelineDateToTime(2010, 6, 5), "Married", {});
-        bg.TimelineAddStreamEvent(rik_stream, bg.TimelineDateToTime(2011, 16, 12), "Daughter Born", {data:"hi"});
+        bg.TimelineAddStreamEvent(rik_stream, bg.TimelineDateToTime(3, 10, 1982), "Born", {});
+        bg.TimelineAddStreamEvent(rik_stream, bg.TimelineDateToTime(5, 6, 2010), "Married", {});
+        bg.TimelineAddStreamEvent(rik_stream, bg.TimelineDateToTime(12, 16, 2011), "Daughter Born", {data:"hi"});
 
 		var bob_stream = bg.TimelineCreateStream(timeline, "Bob", {age:25});
         bg.TimelineAddStreamEvent(bob_stream, 10, "Test", {});
