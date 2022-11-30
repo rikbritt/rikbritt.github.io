7eb0cc8a8b24d60449c005686ef4697b54909898
rikbritt <richard.britt@live.co.uk>
2022-11-30 20:01:19 +0000

    fix

--- webui/imgui_hierarchies.js
+++ webui/imgui_hierarchies.js
@@ -201,7 +201,7 @@ function UpdateHierarchyEditor()
 				var node_a = hierarchy_instance.hierarchyNodes[hierarchy_editor_instance.selected_node_a.idx];
 				var node_a_output_name = Object.entries(node_a.generator.outputs)[hierarchy_editor_instance.selected_node_a.output_pin][0];
 				var node_b = hierarchy_instance.hierarchyNodes[hierarchy_editor_instance.selected_node_b.idx];
-				var node_b_input_name = Object.entries(node_b.generator.outputs)[hierarchy_editor_instance.selected_node_b.input_pin][0];
+				var node_b_input_name = Object.entries(node_b.generator.inputs)[hierarchy_editor_instance.selected_node_b.input_pin][0];
 				bg.CreateGenerationHierarchyLink(node_a, node_a_output_name, node_b, node_b_input_name);
 			}
 
