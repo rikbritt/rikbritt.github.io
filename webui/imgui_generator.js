7ddfe853fa6f8829eeb2b3288105db2251ab28f5
rikbritt <richard.britt@live.co.uk>
2023-01-07 11:03:35 +0000

    script error reported

--- webui/imgui_generator.js
+++ webui/imgui_generator.js
@@ -442,9 +442,7 @@ function UpdateGeneratorWindow(close_func, generator)
 				} 
 				catch (error) 
 				{
-					console.error(error);
-					// expected output: ReferenceError: nonExistentFunction is not defined
-					// (Note: the exact output may be browser-dependent)
+					gLastScriptErrors[generator.id] = error.toString();
 				}
 			}
 			if(gLastScriptErrors[generator.id] != null)
