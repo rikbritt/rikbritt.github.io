2c8eba18b255ce4d0fa2a0f8654679ab1ad4c2af
rikbritt <richard.britt@live.co.uk>
2023-01-17 08:20:45 +0000

    Making UpdateParamEditor use registered imgui edit value funcs

--- webui/imgui_fieldtypes.js
+++ webui/imgui_fieldtypes.js
@@ -2,7 +2,8 @@
 gFieldTypesImGui = {};
 
 //imgui_edit_field_func = add imgui controls to edit the field setup (min/max/etc)
-function SetupFieldTypeImGui(field_type, imgui_edit_field_func)
+//imgui_edit_val_func = add imgui contrlols to edit a value of an instance of the field
+function SetupFieldTypeImGui(field_type, imgui_edit_field_func, imgui_edit_val_func)
 {
     if(bg.dataTypes[field_type] == null)
     {
@@ -12,7 +13,8 @@ function SetupFieldTypeImGui(field_type, imgui_edit_field_func)
 
     gFieldTypesImGui[field_type] = 
     {
-        edit_field_imgui:imgui_edit_field_func
+        edit_field_imgui:imgui_edit_field_func,
+        edit_value_imgui:imgui_edit_val_func
     };
 
     return true;
@@ -20,11 +22,166 @@ function SetupFieldTypeImGui(field_type, imgui_edit_field_func)
 
 function Field_FloatMinMaxEdit(field_name, field_data)
 {
-	ImGui.SliderFloat(field_name, (_ = field_data.min) => field_data.min = _, 0, 1000);
-	ImGui.SliderFloat(field_name, (_ = field_data.max) => field_data.max = _, 0, 1000);		
+	ImGui.SliderFloat(`${field_name}##min`, (_ = field_data.min) => field_data.min = _, 0, 1000);
+	ImGui.SliderFloat(`${field_name}##max`, (_ = field_data.max) => field_data.max = _, 0, 1000);		
+}
+
+function FieldVal_FloatEdit(field_name, field_data, get_func, set_func)
+{
+    ImGui.SliderFloat(field_name, (_ = get_func()) => set_func(_), field_data.min, field_data.max);		
+}
+
+function Field_IntMinMaxEdit(field_name, field_data)
+{
+	ImGui.SliderInt(`${field_name}##min`, (_ = field_data.min) => field_data.min = _, 0, 1000);
+	ImGui.SliderInt(`${field_name}##max`, (_ = field_data.max) => field_data.max = _, 0, 1000);		
+}
+function FieldVal_IntEdit(field_name, field_data, get_func, set_func)
+{
+    ImGui.SliderInt(field_name, (_ = get_func()) => set_func(_), field_data.min, field_data.max);
 }
 
 SetupFieldTypeImGui("norm", function() { });
-SetupFieldTypeImGui("float", Field_FloatMinMaxEdit);
-SetupFieldTypeImGui("distance", Field_FloatMinMaxEdit);
-SetupFieldTypeImGui("time", Field_FloatMinMaxEdit);
+SetupFieldTypeImGui("bool",
+    function() { },
+    function(field_name, field_data, get_func, set_func)
+    {
+        ImGui.Checkbox(field_name, (_ = get_func()) => set_func(_));
+    }
+);
+
+SetupFieldTypeImGui("text", 
+    function() { },
+    function(field_name, field_data, get_func, set_func)
+    {
+		ImGui.InputText(field_name, (_ = get_func()) => set_func(_), 256);
+    }
+);
+
+SetupFieldTypeImGui("float", Field_FloatMinMaxEdit, FieldVal_FloatEdit);
+SetupFieldTypeImGui("distance", Field_FloatMinMaxEdit, FieldVal_FloatEdit);
+SetupFieldTypeImGui("time", Field_FloatMinMaxEdit, FieldVal_FloatEdit);
+SetupFieldTypeImGui("int", Field_IntMinMaxEdit, FieldVal_IntEdit);
+SetupFieldTypeImGui("list", 
+    function(field_name, field_data) 
+    {
+        ImGui.Text("TODO - list");
+        // var list = getFunc();
+        // if(Array.isArray(list) == false)
+        // {
+        // 	ImGui.Text(`${field_name} is not a JS Array!`);
+        // }
+        // else
+        // {
+        // 	if(ImGui.TreeNodeEx(`${field_name} ${list.length} / ${field_data.max}`, ImGui.TreeNodeFlags.DefaultOpen))
+        // 	{
+        // 		for(var i=0; i<list.length; ++i)
+        // 		{
+        // 			ImGui.PushID(i);
+        // 			if(ImGui.Button("Del"))
+        // 			{
+        // 				list.splice(i, 1);
+        // 				--i;
+        // 			}
+        // 			else
+        // 			{
+        // 				ImGui.SameLine();
+        // 				ImGui.Text(`${i} :`);
+        // 				UpdateParamEditor(
+        // 					field_data.elementType,
+        // 					function() { var l = list; var idx = i; return function () { 
+        // 						return l[idx];
+        // 					 } }(),
+        // 					function() { var l = list; var idx = i; return function (val) {
+        // 						l[idx] = val;
+        // 						return val;
+        // 					} }(),
+        // 					`${i}`
+        // 				);
+        // 			}
+        // 			ImGui.PopID();
+        // 		}
+        // 		if(list.length < field_data.max && ImGui.Button("Add Element"))
+        // 		{
+        // 			list.push(GetParamDefault(field_data.elementType));
+        // 		}
+        // 	}
+        // }
+    },
+    function(field_name, field_data, get_func, set_func)
+    {
+        
+		var list = get_func();
+		if(Array.isArray(list) == false)
+		{
+			ImGui.Text(`${field_name} is not a JS Array!`);
+		}
+		else
+		{
+			if(ImGui.TreeNodeEx(`${field_name} ${list.length} / ${field_data.max}`, ImGui.TreeNodeFlags.DefaultOpen))
+			{
+				for(var i=0; i<list.length; ++i)
+				{
+					ImGui.PushID(i);
+					if(ImGui.Button("Del"))
+					{
+						list.splice(i, 1);
+						--i;
+					}
+					else
+					{
+						ImGui.SameLine();
+						ImGui.Text(`${i} :`);
+						UpdateParamEditor(
+							field_data.elementType,
+							function() 
+                            { 
+                                var l = list; 
+                                var idx = i; 
+                                return function () 
+                                { 
+								    return l[idx];
+							    } 
+                            }(),
+							function()
+                            { 
+                                var l = list; 
+                                var idx = i; 
+                                return function (val) 
+                                {
+								    l[idx] = val;
+								    return val;
+							    }
+                            }(),
+							`${i}`
+						);
+					}
+					ImGui.PopID();
+				}
+				if(list.length < field_data.max && ImGui.Button("Add Element"))
+				{
+					list.push(GetParamDefault(field_data.elementType));
+				}
+			}
+		}
+    }
+);
+ 
+SetupFieldTypeImGui("data", 
+    function(field_name, field_data) {
+        ImGui.Text("TODO - data");
+        //if(ImGui.TreeNodeEx(field_name, ImGui.TreeNodeFlags.DefaultOpen))
+        //{
+        //	UpdateGeneratorInputsImGui(field_data.dataType.fields, getFunc());
+        //	ImGui.TreePop();
+        //}
+    },
+    function(field_name, field_data, get_func, set_func)
+    {
+		if(ImGui.TreeNodeEx(field_name, ImGui.TreeNodeFlags.DefaultOpen))
+		{
+			UpdateGeneratorInputsImGui(field_data.dataType.fields, get_func());
+			ImGui.TreePop();
+		}
+    }
+ );
--- webui/imgui_params.js
+++ webui/imgui_params.js
@@ -32,75 +32,10 @@ function GetParamDefault(paramData)
 
 function UpdateParamEditor(paramData, getFunc, setFunc, paramKey)
 {
-	if(paramData.type == "float" 
-	|| paramData.type == "distance"
-	|| paramData.type == "time")
+    var field_imgui = gFieldTypesImGui[paramData.type];
+	if(field_imgui != null)
 	{
-		ImGui.SliderFloat(paramKey, (_ = getFunc()) => setFunc(_), paramData.min, paramData.max);		
-	}
-	else if(paramData.type == "int")
-	{
-		ImGui.SliderInt(paramKey, (_ = getFunc()) => setFunc(_), paramData.min, paramData.max);
-	}
-	else if(paramData.type == "data")
-	{
-		if(ImGui.TreeNodeEx(paramKey, ImGui.TreeNodeFlags.DefaultOpen))
-		{
-			UpdateGeneratorInputsImGui(paramData.dataType.fields, getFunc());
-			ImGui.TreePop();
-		}
-	}
-	else if(paramData.type == "bool")
-	{
-		ImGui.Checkbox(paramKey, (_ = getFunc()) => setFunc(_));
-	}
-	else if(paramData.type == "text")
-	{
-		ImGui.InputText(paramKey, (_ = getFunc()) => setFunc(_), 256);
-	}
-	else if(paramData.type == "list")
-	{
-		var list = getFunc();
-		if(Array.isArray(list) == false)
-		{
-			ImGui.Text(`${paramKey} is not a JS Array!`);
-		}
-		else
-		{
-			if(ImGui.TreeNodeEx(`${paramKey} ${list.length} / ${paramData.max}`, ImGui.TreeNodeFlags.DefaultOpen))
-			{
-				for(var i=0; i<list.length; ++i)
-				{
-					ImGui.PushID(i);
-					if(ImGui.Button("Del"))
-					{
-						list.splice(i, 1);
-						--i;
-					}
-					else
-					{
-						ImGui.SameLine();
-						ImGui.Text(`${i} :`);
-						UpdateParamEditor(
-							paramData.elementType,
-							function() { var l = list; var idx = i; return function () { 
-								return l[idx];
-							 } }(),
-							function() { var l = list; var idx = i; return function (val) {
-								l[idx] = val;
-								return val;
-							} }(),
-							`${i}`
-						);
-					}
-					ImGui.PopID();
-				}
-				if(list.length < paramData.max && ImGui.Button("Add Element"))
-				{
-					list.push(GetParamDefault(paramData.elementType));
-				}
-			}
-		}
+		field_imgui.edit_value_imgui(paramKey, paramData, getFunc, setFunc);
 	}
 	else
 	{
