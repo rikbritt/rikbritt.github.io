18897c941123e1bd3deb6b9bd6c4352e3ad630d4
rikbritt <richard.britt@live.co.uk>
2023-03-25 17:37:14 +0000

    woodWorkingDataDef info

--- generators/construction/woodworking/woodworking_datadefs.js
+++ generators/construction/woodworking/woodworking_datadefs.js
@@ -2,6 +2,8 @@
 var woodWorkingDataDef = {
 	version:1,
 	name:"Wood Working",
+	description:"Info about plank sizes etc",
+	category:["Construction","Wood Working"],
 	fields:[
 		{ name:"standardLumberSizes", 	type:"distance", units:"m", min:0.5, max:20	}, //would be nice to make this tend toward whole units
 		{ name:"plankWidth",			type:"distance", units:"m", min:0.05, max:0.4 }
