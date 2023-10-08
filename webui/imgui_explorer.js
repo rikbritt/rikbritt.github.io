9df9c4d7b6c745adea28760f73c4d4c8eef43df5
rikbritt <richard.britt@live.co.uk>
2023-10-08 21:12:05 +0100

    Add Push/PopID to Explorer Window project list

--- webui/imgui_explorer.js
+++ webui/imgui_explorer.js
@@ -12,6 +12,7 @@ function UpdateExplorerWindow( close_func, data )
         for(var i=0; i<bg.projects.length; ++i)
         {
             var project = bg.projects[i];
+            ImGui.PushID(i);
             ImGui.Separator();
             ImGui.Text("Project - " + project.name);
             if(ImGui.Button("Save Project (TODO"))
@@ -23,9 +24,12 @@ function UpdateExplorerWindow( close_func, data )
             {
                 bg.projects.splice(i, 1);
                 i -= 1;
-                continue;
             }
-            UpdateProjectProperties(bg.projects[i]);
+            else
+            {
+                UpdateProjectProperties(bg.projects[i]);
+            }
+            Imgui.PopID();
         }
     }
 }
