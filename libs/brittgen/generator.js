30ab8d945678ef0cf9db969943713a73841fac56
rikbritt <richard.britt@live.co.uk>
2023-12-05 20:11:14 +0000

    SaveGeneratorToJS ws

--- libs/brittgen/generator.js
+++ libs/brittgen/generator.js
@@ -352,6 +352,7 @@ bg.SaveGeneratorToJS = function(generator)
 	var generator_json = JSON.stringify(generator, null, 4);
 	generator_json = generator_json.replaceAll("\n", "\n    ");
 	var generator_script_func = generator.script.toString();
+	generator_script_func = generator_script_func.replaceAll("\t", "    ");
 	var gen_js = `bg.LoadGenerator(\n    ${generator_json},\n    ${generator_script_func}\n)`;
 	return gen_js;
 }
