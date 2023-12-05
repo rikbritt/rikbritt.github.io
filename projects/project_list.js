d6cd44d33613ed466b33d0cede4c117064738eb5
rikbritt <richard.britt@live.co.uk>
2023-12-05 19:11:19 +0000

    adding common project

--- projects/project_list.js
+++ projects/project_list.js
@@ -1,3 +1,4 @@
 AddProjectToListFromURL("projects/global");
+AddProjectToListFromURL("projects/common");
 AddProjectToListFromURL("projects/murder_mystery");
 AddProjectToListFromURL("projects/Tests");
