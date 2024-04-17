93e77788377758fd6771f07685c837ee778889d0
rikbritt <richard.britt@live.co.uk>
2024-04-17 21:28:46 +0100

    add CreateEmptyDataTable and RegisterProjectDataTable etc

---
+++ libs/brittgen/datatable.js
@@ -0,0 +1,36 @@
+
+bg.UpgradeDataTable = function(data_table)
+{
+
+}
+
+bg.ValidateDataTable = function(data_table)
+{
+    return true;
+}
+
+bg.RegisterProjectDataTable = function(project, data_table)
+{
+	bg.UpgradeDataTable(data_table);
+	if(bg.ValidateDataTable(data_table))
+	{
+		project.dataTables[data_table.id] = data_table;
+		AssetDb.AddAsset(project.assetDb, data_table.id, "data_table", data_table);
+		return true;
+	}
+	return false;
+}
+
+bg.CreateEmptyDataTable = function(project)
+{
+	var data_table = {
+		version:1,
+		id:bg.CreateGUID(),
+		name:"New Data Table",
+		data:[]
+	};
+
+	bg.RegisterProjectDataTable(project, data_table);
+
+	return data_table;
+}
