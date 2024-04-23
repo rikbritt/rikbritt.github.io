493f4f7a77fe73c99e14f067db292476a06a6825
rikbritt <richard.britt@live.co.uk>
2024-04-23 20:24:04 +0100

    split name_generator into two files

--- generators/information/name_v1_generator.js
+++ generators/information/name_v1_generator.js
@@ -1,25 +1,3 @@
-var nameV2Generator = {
-	version:1,
-	name:"Name V2",
-	description:"Does nothing, just generates an empty string right now",
-	category:["Information"],
-	inputs:{
-		name:"inputs",
-		version:1,
-		fields:[]
-	},
-	outputs:{
-		name:"outputs",
-		version:1,
-		fields:[
-			{ name:"data",		type:"text"	}
-		],
-	},
-	script:function(inputs, outputs){
-		outputs.data = "";
-	}
-}
-bg.RegisterProjectGenerator(bg.global_project, nameV2Generator);
 
 var nameV1Generator = {
 	version:1,
---
+++ generators/information/name_v2_generator.js
@@ -0,0 +1,22 @@
+var nameV2Generator = {
+	version:1,
+	name:"Name V2",
+	description:"Does nothing, just generates an empty string right now",
+	category:["Information"],
+	inputs:{
+		name:"inputs",
+		version:1,
+		fields:[]
+	},
+	outputs:{
+		name:"outputs",
+		version:1,
+		fields:[
+			{ name:"data",		type:"text"	}
+		],
+	},
+	script:function(inputs, outputs){
+		outputs.data = "";
+	}
+}
+bg.RegisterProjectGenerator(bg.global_project, nameV2Generator);
