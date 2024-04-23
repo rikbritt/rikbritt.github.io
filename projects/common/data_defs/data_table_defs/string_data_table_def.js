872090450adcbd77fb8e3bc59f8aea3e6d2517ab
rikbritt <richard.britt@live.co.uk>
2024-04-23 20:45:31 +0100

    add name vowel data table

--- projects/common/data_defs/data_table_defs/string_data_table_def.js
+++ projects/common/data_defs/data_table_defs/string_data_table_def.js
@@ -1,6 +1,7 @@
 
 var string_data_table_def = {
 	version:1,
+	id:"68409ea6-d39b-49ba-ab87-a6bb9db1b9c2",
 	name:"String Data Table Def",
 	description:"Used for data tables that are just a string array",
 	category:["DataTableDefs"],
