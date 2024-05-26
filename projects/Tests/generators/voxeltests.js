33d8c42beb400fa5608fb241e1d5216a2b8683b6
rikbritt <richard.britt@live.co.uk>
2024-05-26 16:49:46 +0100

    voxeltests.js loading

--- projects/Tests/generators/voxeltests.js
+++ projects/Tests/generators/voxeltests.js
@@ -1,45 +1,44 @@
-
-var testVoxelsGenerator = {
-	version:1,
-	name:"Voxels",
-	category:["Test"],
-	inputs:{
-		name:"inputs",
+bg.LoadGeneratorFromObject( 
+	{
 		version:1,
-		fields:[
-			{ name:"width",		type:"distance",	units:"m",	min:1.0, max:5 },
-			{ name:"depth",		type:"distance",	units:"m",	min:1.0, max:5 },
-			{ name:"height",	type:"distance",	units:"m",	min:1.0, max:5 }
-		],
-	},
-	outputs:{
-		name:"outputs",
-		version:1,
-		fields:[
-			{ name:"model",			type:"model"	}
-		],
-	},
-	script:function(inputs, outputs){
-		outputs.model = bg.CreateNode();
-		
-		var segmentsWidth = Math.floor(inputs.width);
-		var segmentsHeight = Math.floor(inputs.height);
-		var segmentsDepth = Math.floor(inputs.depth);
-				
-		var voxelData = {
-			width:segmentsWidth,
-			depth:segmentsDepth,
-			height:segmentsHeight,
-			values:[]
-		};
-		
-		for(var i=0; i<(voxelData.width*voxelData.depth*voxelData.height);++i)
-		{
-			voxelData.values.push(bg.GetRandomInt(0,1));
+		name:"Voxels",
+		category:["Test"],
+		inputs:{
+			name:"inputs",
+			version:1,
+			fields:[
+				{ name:"width",		type:"distance",	units:"m",	min:1.0, max:5 },
+				{ name:"depth",		type:"distance",	units:"m",	min:1.0, max:5 },
+				{ name:"height",	type:"distance",	units:"m",	min:1.0, max:5 }
+			],
+		},
+		outputs:{
+			name:"outputs",
+			version:1,
+			fields:[
+				{ name:"model",			type:"model"	}
+			],
+		},
+		script:function(inputs, outputs){
+			outputs.model = bg.CreateNode();
+			
+			var segmentsWidth = Math.floor(inputs.width);
+			var segmentsHeight = Math.floor(inputs.height);
+			var segmentsDepth = Math.floor(inputs.depth);
+					
+			var voxelData = {
+				width:segmentsWidth,
+				depth:segmentsDepth,
+				height:segmentsHeight,
+				values:[]
+			};
+			
+			for(var i=0; i<(voxelData.width*voxelData.depth*voxelData.height);++i)
+			{
+				voxelData.values.push(bg.GetRandomInt(0,1));
+			}
+			
+			bg.CreateVoxelModel(outputs.model, inputs.width, inputs.height, inputs.depth, voxelData);
 		}
-		
-		bg.CreateVoxelModel(outputs.model, inputs.width, inputs.height, inputs.depth, voxelData);
 	}
-
-}
-bg.RegisterProjectGenerator(bg.global_project, testVoxelsGenerator);
+);
