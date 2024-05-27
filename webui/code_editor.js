7c9366be8888d60c426036807ba94cb43d43d8f2
rikbritt <richard.britt@live.co.uk>
2024-05-27 11:37:13 +0100

    testing external code editor

--- webui/code_editor.js
+++ webui/code_editor.js
@@ -6,6 +6,9 @@ function SetupCodeEditor()
     gCodeEditor = ace.edit("editor");
     gCodeEditor.setTheme("ace/theme/monokai");
     gCodeEditor.session.setMode("ace/mode/javascript");
+    
+    window.open("code_editor.html", "game", "status=0,location=0,toolbar=0,menubar=0,addressbar=0,width=1280,height=720");
+
 }
 
 function SetCodeToEdit(code_prop, name)
