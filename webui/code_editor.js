e0f36a4d841ec640c67e210956ddef7392c7ff18
rikbritt <richard.britt@live.co.uk>
2024-05-28 22:40:52 +0100

    better reopen code editor

--- webui/code_editor.js
+++ webui/code_editor.js
@@ -4,11 +4,7 @@ var gCodeEditorWindow = 0;
 var gCurrentCodeProp = null;
 
 function SetupCodeEditor()
-{
-    // gCodeEditor = ace.edit("editor");
-    // gCodeEditor.setTheme("ace/theme/monokai");
-    // gCodeEditor.session.setMode("ace/mode/javascript");
-    
+{    
     gCodeEditorWindow = window.open("code_editor.html", "code_editor", "status=0,location=0,toolbar=0,menubar=0,addressbar=0,width=1280,height=720");
 
     
@@ -27,16 +23,6 @@ function SetCodeToEdit(code_prop, name)
 {
     gCurrentCodeProp = code_prop;
     gCodeEditorWindow.postMessage({code:code_prop(), name:name}, "*");
-
-    // if(gCodeEditor.changeListener != null)
-    // {
-    //     gCodeEditor.session.removeListener('change', gCodeEditor.changeListener);
-    // }
-    // gCodeEditor.session.setValue(code_prop());
-    // document.getElementById("editor_info_script_name").innerText = name;
-    // gCodeEditor.changeListener = gCodeEditor.session.on('change', function() {
-    //     code_prop(gCodeEditor.session.getValue());
-    //   });
 }
 
 function UpdateCodeEditorOptions()
@@ -44,5 +30,9 @@ function UpdateCodeEditorOptions()
     if(ImGui.MenuItem("Reopen Code Editor Window"))
     {
         gCodeEditorWindow = window.open("code_editor.html", "code_editor", "status=0,location=0,toolbar=0,menubar=0,addressbar=0,width=1280,height=720");
+        if(gCurrentCodeProp)
+        {
+            SetCodeToEdit(gCurrentCodeProp, "?");
+        }
     }
 }
