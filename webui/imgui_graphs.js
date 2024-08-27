f3cfa8455330e1de6bc4f30a22946e2593bcca8b
rikbritt <richard.britt@live.co.uk>
2024-08-27 21:25:53 +0100

    clean up

--- webui/imgui_graphs.js
+++ webui/imgui_graphs.js
@@ -218,155 +218,29 @@ function UpdateSelectedNodeInfo(selected_node, graph_instance)
 	ImGui.PushID(selected_node.name);
 	ImGui.Separator();
 	ImGui.Text(selected_node.name);
-	ImGui.SliderInt("Selected Node " + selected_node.name, (_ = selected_node.idx) => selected_node.idx = _, 0, graph_instance.nodes.length-1);
-	if(selected_node.idx >= 0 && selected_node.idx < graph_instance.nodes.length)
-	{
-		var selected_graph_node = graph_instance.nodes[selected_node.idx];
-		var selected_node_layout = bg.FindOrCreateNodeLayout(graph_instance.layout, selected_node.idx);
-		ImGui.Text("Name : " + bg.GetGenerationGraphNodeName(selected_graph_node));
-		ImGui.SliderInt("X", (_ = selected_node_layout.x) => selected_node_layout.x = _, -100, 500);
-		ImGui.SliderInt("Y", (_ = selected_node_layout.y) => selected_node_layout.y = _, -100, 500);
-		ImGui.Text("Inputs : ");
-		ImGui.Indent();
+	// ImGui.SliderInt("Selected Node " + selected_node.name, (_ = selected_node.idx) => selected_node.idx = _, 0, graph_instance.nodes.length-1);
+	// if(selected_node.idx >= 0 && selected_node.idx < graph_instance.nodes.length)
+	// {
+	// 	var selected_graph_node = graph_instance.nodes[selected_node.idx];
+	// 	var selected_node_layout = bg.FindOrCreateNodeLayout(graph_instance.layout, selected_node.idx);
+	// 	ImGui.Text("Name : " + bg.GetGenerationGraphNodeName(selected_graph_node));
+	// 	ImGui.SliderInt("X", (_ = selected_node_layout.x) => selected_node_layout.x = _, -100, 500);
+	// 	ImGui.SliderInt("Y", (_ = selected_node_layout.y) => selected_node_layout.y = _, -100, 500);
+	// 	ImGui.Text("Inputs : ");
+	// 	ImGui.Indent();
 
-		var nodeType = gGraphNodeTypes[selected_graph_node.type];
-		if(nodeType != null && nodeType.update_node_info != null)
-		{
-			nodeType.update_node_info(graph_instance, selected_graph_node);
-		}
-	}
-	else
-	{
-		ImGui.Text("Invalid Node");
-	}
+	// 	var nodeType = gGraphNodeTypes[selected_graph_node.type];
+	// 	if(nodeType != null && nodeType.update_node_info != null)
+	// 	{
+	// 		nodeType.update_node_info(graph_instance, selected_graph_node);
+	// 	}
+	// }
+	// else
+	// {
+	// 	ImGui.Text("Invalid Node");
+	// }
 	ImGui.PopID();
 }
-
-//var c_x = 0;
-//var c_y = 0;
-//function UpdateGraphEditor()
-//{
-//	if(gShowGraphEditor)
-//	{
-//		if(gGraphInstances.length == 0)
-//		{
-//			CreateGraphEditor("New Graph");
-//		}
-//
-//		var graph_editor_instance = gGraphInstances[0];
-//		var graph_instance = gGraphInstances[0].instance;
-//		if(ImGui.Begin("Graph Editor - " + graph_instance.name))
-//		{
-//			var win_width = ImGui.GetContentRegionAvail().x;
-//			var win_height = ImGui.GetContentRegionAvail().y;
-//			var gens_width = Math.min(200, win_width * 0.5);
-//
-//			ImGui.BeginChild("GraphProperties", new ImGui.Vec2(gens_width, win_height))
-//			ImGui.Text(graph_instance.name);
-//
-//			if(ImGui.Button("Create Test Setup"))
-//			{
-//				var person_node = bg.CreateGenerationGraphNode(gGraphInstances[0].instance, mm_personGenerator);
-//				gGraphInstances[0].node_positions.push({x:0,y:10});
-//
-//				var suspect_node = bg.CreateGenerationGraphNode(gGraphInstances[0].instance, mm_suspectGenerator);
-//				gGraphInstances[0].node_positions.push({x:300,y:10});
-//
-//				bg.CreateGenerationGraphLink(person_node, "data", suspect_node, "suspect");
-//			}
-//
-//			ImGui.Text("Num Nodes : " + graph_instance.nodes.length);
-//			ImGui.Indent();
-//			for(var i=0; i<graph_instance.nodes.length; ++i)
-//			{
-//				var node = graph_instance.nodes[i];
-//				ImGui.Text(node.generator.name);
-//			}
-//			ImGui.Unindent();
-//
-//			UpdateSelectedNodeInfo(graph_editor_instance.selected_node_a, graph_editor_instance);
-//			UpdateSelectedNodeInfo(graph_editor_instance.selected_node_b, graph_editor_instance);
-//
-//			if(ImGui.Button("Add A:Output To B:Input Link"))
-//			{
-//				var node_a = graph_instance.nodes[graph_editor_instance.selected_node_a.idx];
-//				var node_a_output_name = Object.entries(node_a.generator.outputs)[graph_editor_instance.selected_node_a.output_pin][0];
-//				var node_b = graph_instance.nodes[graph_editor_instance.selected_node_b.idx];
-//				var node_b_input_name = Object.entries(node_b.generator.inputs)[graph_editor_instance.selected_node_b.input_pin][0];
-//				bg.CreateGenerationGraphLink(node_a, node_a_output_name, node_b, node_b_input_name);
-//			}
-//
-//			ImGui.SliderInt("Canvas X", (_ = c_x) => c_x = _, 0, 1000);
-//			ImGui.SliderInt("Canvas Y", (_ = c_y) => c_y = _, 0, 1000);
-//			ImGui.EndChild();
-//
-//			ImGui.SameLine();
-//
-//			var dw = ImGui.GetWindowDrawList();
-//
-//			NodeImGui.BeginCanvas("canvas",  new ImGui.Vec2(win_width - gens_width, win_height));
-//			NodeImGui.Current_Canvas.Scrolling.x = c_x;
-//			NodeImGui.Current_Canvas.Scrolling.y = c_y;
-//			for(var i=0; i<graph_instance.nodes.length; ++i)
-//			{
-//				var node = graph_instance.nodes[i];
-//				NodeImGui.BeginNode(
-//					node.idx,
-//					node.generator.name,
-//					graph_editor_instance.node_positions[i].x,
-//					graph_editor_instance.node_positions[i].y
-//				);
-//
-//				for([paramKey, paramData] of Object.entries(node.generator.inputs))
-//				{
-//					NodeImGui.InputPin(paramKey);
-//				}
-//
-//				for([paramKey, paramData] of Object.entries(node.generator.outputs))
-//				{
-//					NodeImGui.OutputPin(paramKey);
-//				}
-//
-//				//Input Links
-//				for(var j=0; j<node.inputs.length; ++j)
-//				{
-//					var link = node.inputs[j];
-//					NodeImGui.LinkNode(
-//						link.fromNodeIdx,
-//						link.fromNodeOutputName,
-//						link.toNodeInputName
-//					);
-//				}
-//
-//				NodeImGui.EndNode();
-//			}
-//
-//			if (ImGui.BeginPopupContextWindow())
-//			{
-//				if(ImGui.BeginMenu("Add Generator..."))
-//				{
-//					UpdateGeneratorsList(
-//						function(generator)
-//						{
-//							bg.CreateGenerationGraphNode(gGraphInstances[0].instance, generator);
-//							gGraphInstances[0].node_positions.push({x:0,y:0});
-//						}
-//					);
-//					ImGui.EndMenu();
-//				}
-//
-//				if(ImGui.MenuItem("Add Note / Comment"))
-//				{
-//				}
-//
-//				ImGui.EndPopup();
-//			}
-//
-//			NodeImGui.EndCanvas();
-//		}
-//		ImGui.End();
-//	}
-//}
 
 function ProcessGraphConnection(graph, connection)
 {
@@ -708,24 +582,6 @@ function UpdateGenGraphWindow(close_func, graph_instance)
 		UpdateSelectedNodeInfo(current_canvas.selected_node_a, graph_instance);
 		UpdateSelectedNodeInfo(current_canvas.selected_node_b, graph_instance);
 		
-		if(ImGui.Button("Make Link From A Output to B Input"))
-		{
-			var a_node = graph_instance.nodes[current_canvas.selected_node_a.idx];
-			var a_generator = AssetDb.GetAsset(gAssetDb, a_node.asset_id, a_node.type);
-			var a_sub_idx = current_canvas.selected_node_a.output_pin;
-
-			var b_node = graph_instance.nodes[current_canvas.selected_node_b.idx];
-			var b_generator = AssetDb.GetAsset(gAssetDb, b_node.asset_id, b_node.type);
-			var b_sub_idx = current_canvas.selected_node_b.input_pin;
-
-			//TODO - need to make the sub indexes know if its input or output pins
-			// maybe use negative numbers, or put a 'type' with the sub idx?
-			bg.AddGraphSubEdgeById(graph_instance, a_node.id, a_sub_idx, b_node.id, b_sub_idx);
-		}
-		// if(ImGui.Button("Make Link From B Output to A Input"))
-		// {
-			
-		// }
 
 		ImGui.SliderInt("Canvas X", (_ = current_canvas.c_x) => current_canvas.c_x = _, 0, 1000);
 		ImGui.SliderInt("Canvas Y", (_ = current_canvas.c_y) => current_canvas.c_y = _, 0, 1000);
