2f9ce502ffa1e7c29e901e66fb9c72fbe5714b01
rikbritt <richard.britt@live.co.uk>
2024-09-21 23:15:05 +0100

    added copy_int_val generator

---
+++ projects/Tests/generators/unittests/copy_int_val.js
@@ -0,0 +1,24 @@
+export var generator = {
+	version:1,
+	name:"Copy Int A",
+	description:"For Unit tests.",
+	category:["Test"],
+	inputs:{
+		name:"inputs",
+		version:1,
+		fields:[
+			{ name:"int_val",	type:"int",					min:0,		max:10}
+        ],
+	},
+	outputs:{
+		name:"outputs",
+		version:1,
+		fields:[
+			{ name:"int_out",	type:"int"	}
+		],
+	},
+	script:function(inputs, outputs)
+    {
+       outputs.int_out = inputs.int_val;        
+	}
+};
